f6e2507fdce92b0eb4137a0e18279102ad088a68
Pascal Venema <36227438+pascalvenema@users.noreply.github.com>
2025-06-10 17:11:46 +0200

    update

--- ehrshot/bash_scripts/7__eval_helper.sh
+++ ehrshot/bash_scripts/7__eval_helper.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=7__eval_helper
 #SBATCH --output=logs/7__eval_helper_%A.out
 #SBATCH --error=logs/7__eval_helper_%A.err
-#SBATCH --time=2-00:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus-per-node=2
 #SBATCH --mem=200G
